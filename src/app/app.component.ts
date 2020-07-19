53b32093e2c066e51ff380e256be6f137d3c2e8d
nareshrouthu <naresh.routhu19@gmail.com>
2020-07-19 16:48:17 +0530

    5 files modified, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Dear User, Welcome to Beta Testing';
+  name = 'Dear User!!';
 }
